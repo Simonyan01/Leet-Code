b4bc4e24b408a1483e2fac86abdf8023b997111f
Simonyan01 <lyov.simonyan.01@gmail.com>
2024-12-23 01:36:29 +0400

    #12 and #13 are ready

---
+++ #67.js
@@ -0,0 +1,24 @@
+let roman = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000,
+}
+
+let romanToInteger = function (str) {
+    let count = 0
+
+    for (let i = 0; i < str.length; ++i) {
+        if (roman[str[i]] < roman[str[i + 1]]) {
+            count -= roman[str[i]] // 0 - I(1) = -1
+        } else {
+            count += roman[str[i]] // -1 + X(10) = 9
+        }
+    }
+
+    return count
+}
+
